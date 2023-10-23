9b97c542c2912508c4377198b6bb390eed1c507b
Djumash <djumash.violin@gmail.com>
2023-10-23 12:25:22 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import { CitiesProvider } from "./context/CitiesContext.jsx";
+import { AuthProvider } from "./context/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
+
+import CityList from "./components/CityList.jsx";
+import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
+import Form from "./components/Form.jsx";
+import SpinnerFullPage from "./components/SpinnerFullPage.jsx";
+
+// dist/index.html                   0.47 kB │ gzip:   0.31 kB
+// dist/assets/index-d70904ab.css   31.32 kB │ gzip:   5.22 kB
+// dist/assets/index-3c1ff039.js   537.11 kB │ gzip: 157.87 kB
+
+// import Product from "./pages/Product.jsx";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Product = lazy(() => import("./pages/Product"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+
+function App() {
+  return (
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ src/readme.md
@@ -0,0 +1,3 @@
+# The World Traveller
+
+A world traveller single page application, an easy way to keep track of your travels.
